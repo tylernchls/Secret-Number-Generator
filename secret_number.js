c913dbf4e961d01d40c8648f52fdabd9e13845a6
Tyler Nichols <tylernchls@gmail.com>
2016-12-06 17:44:31 -1000

    finished

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,15 @@
 'use strict';
 module.exports = function() {
-  
+
+//private variable
+ var number = Math.random();
+
+//private method
+function secretNumber(){
+ return number;
+}
+
+// public return to expose whats in side
+return secretNumber;
+
 };
